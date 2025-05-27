06badf8b41131276c34ab1f01ca09f3ee23dcc3c
marhaasa <marius@aasarod.no>
2025-05-27 22:29:09 +0200

    Update disabled and correct telescope

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,22 +1,14 @@
 return {
-  { "echasnovski/mini.pairs", enabled = false },
+  -- Core UI plugins to disable
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  
-  -- Disable ALL mini.nvim modules to prevent conflicts
-  { "echasnovski/mini.nvim", enabled = false },
-  
-  -- Disable LazyVim plugins that cause horizontal highlights
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "echasnovski/mini.hipatterns", enabled = false },
-  { "RRethy/vim-illuminate", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "echasnovski/mini.cursorword", enabled = false },
-  
-  -- Disable statusline plugins but keep snacks for essential functions
   { "nvim-lualine/lualine.nvim", enabled = false },
   
-  -- Keep mini.starter disabled since we're using minimal snacks
-  { "echasnovski/mini.starter", enabled = false },
+  -- Disable ALL mini.nvim modules (prevents horizontal highlights)
+  { "echasnovski/mini.nvim", enabled = false },
+  
+  -- Disable plugins that cause horizontal highlights
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
