773fb400f4bc3c46646e6c54b8714134da4788cb
marhaasa <marius@aasarod.no>
2025-05-27 20:23:17 +0200

    Add ghostty config

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@
 # create directories
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"/zsh
+mkdir -p "$HOME/Library/Application Support/com.mitchellh.ghostty"
 
 # Symbolic links
 ln -sf "$PWD/.zshrc" "$HOME"/.zshrc
@@ -14,6 +15,7 @@ ln -sf "$PWD/.bash_profile" "$HOME"/.bash_profile  # Keep for compatibility
 ln -sf "$PWD/.bashrc" "$HOME"/.bashrc  # Keep for compatibility
 ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/ghostty/config" "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
 # Note: starship.toml not used with pure prompt in zsh
 
 # notater - use current user's home directory
